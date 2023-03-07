c9069c047384b8e2bdbc6d55649c008cb6259a0e
BCMPF <BCMPF@gmail.com>
2023-03-07 12:08:14 -0500

    add buttons

--- Documents/cursosUdemy/RxJs_Angular_Material/angular-Material/src/app/app.component.ts
+++ Documents/cursosUdemy/RxJs_Angular_Material/angular-Material/src/app/app.component.ts
@@ -1,10 +1,51 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { BreakpointObserver } from '@angular/cdk/layout';
+import { fromEvent, map } from 'rxjs';
+
+export const SCROLL_CONTAINER = 'mat-sidenav-content';
+export const TEXT_LIMIT = 50;
+export const SHADOW_LIMIT = 100;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'angular-Material';
+export class AppComponent implements OnInit{
+
+  public isSmallScreen = false;
+  public popText = false;
+  public applyShadow = false;
+
+  constructor(private breakpointOserver: BreakpointObserver) {}
+
+  ngOnInit(): void {
+    const content = document.getElementsByClassName(SCROLL_CONTAINER)[0];
+
+    console.log(content);
+
+    fromEvent(content, 'scroll')
+      .pipe(
+        map(() => content.scrollTop)
+      )
+      .subscribe({
+        next: (value: number) => this.determineHeader(value)
+      })
+  }
+
+  determineHeader(scrollTop: number) {
+    this.popText = scrollTop >= TEXT_LIMIT;
+    this.applyShadow = scrollTop >= SHADOW_LIMIT;
+  }
+
+
+  ngAfterContentInit(): void {
+    this.breakpointOserver
+    .observe(['(max-width:800px'])
+    .subscribe((res) => this.isSmallScreen = res.matches);
+  }
+
+  get sidenavMode(){
+    return this.isSmallScreen ? 'over' : 'side';
+  }
 }
--- Documents/cursosUdemy/RxJs_Angular_Material/angular-Material/src/app/app.module.ts
+++ Documents/cursosUdemy/RxJs_Angular_Material/angular-Material/src/app/app.module.ts
@@ -5,14 +5,19 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+import { MaterialModule } from './material/material.module';
+import { ButtonsIconsComponent } from './buttons-icons/buttons-icons.component';
+
 @NgModule({
   declarations: [
-    AppComponent
-  ],
+    AppComponent,
+    ButtonsIconsComponent
+   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
