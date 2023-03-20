c149144a04d20edeb021d30da4766f2036899b3d
BCMPF <BCMPF@gmail.com>
2023-03-20 14:38:54 -0400

    angular crud

---
+++ Documents/cursos/Angular13-CRUD/angular13-crud/src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ToolbarComponent } from './layout/toolbar/toolbar.component';
+
+
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ToolbarComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
